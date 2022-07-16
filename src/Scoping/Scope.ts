ef6bf4c52bd6138456363d409299af00efe64073
xeth hung <adamhung12@gmail.com>
2022-07-17 00:51:46 +0800

    update

--- src/Scoping/Scope.ts
+++ src/Scoping/Scope.ts
@@ -1,87 +1,94 @@
 export class Scope<T> implements Scope<T> {
-  constructor(private value: T | null) {
-  }
-
-
-  apply = (op: (t: T) => void): Scope<T> => {
-    if (this.nonEmpty()) {
-      op(this.value!)
+    constructor(private value: T | null) {
     }
-    return this;
-  };
-  ifEmptyApply = (op: () => void): Scope<T> => {
-    if (this.empty()) {
-      op()
+
+    apply = (op: (t: T | null) => void): Scope<T> => {
+        if (this.nonEmpty()) {
+            op(this.value!)
+        }
+        return this;
+    };
+    ifEmptyApply = (op: () => void): Scope<T> => {
+        if (this.empty()) {
+            op()
+        }
+        return this;
+    };
+    ifNonEmptyApply = (op: () => void): Scope<T> => {
+        if (this.nonEmpty()) {
+            op()
+        }
+        return this;
+    };
+
+    ifEmptyProvide = (op: () => T | null): Scope<T> => {
+        if (this.empty()) {
+            return Scope.ofNullable(op())
+        } else {
+            return this;
+        }
+    };
+
+    ifEmpty = (t: T): Scope<T> => {
+        if (this.empty()) {
+            return Scope.ofNullable(t)
+        } else {
+            return this;
+        }
+    };
+
+    public mapTo<O>(op: (t: T) => O | null): Scope<O> {
+        if (this.nonEmpty()) {
+            return Scope.ofNullable<O>(op(this.value!))
+        } else {
+            return Scope.ofNullable<O>(null)
+        }
+    };
+
+    exists = (): boolean => this.nonEmpty();
+    missing = (): boolean => this.empty();
+    nonEmpty = (): boolean => !this.empty();
+
+    empty(): boolean {
+        return this.value === null;
     }
-    return this;
-  };
-  ifNonEmptyApply = (op: () => void): Scope<T> => {
-    if (this.nonEmpty()) {
-      op()
+
+    public static of<U>(v: U): Scope<U> {
+        return Scope.ofNullable(v);
     }
-    return this;
-  };
 
-  ifEmptyProvide = (op: () => T|null): Scope<T> => {
-    if (this.empty()) {
-      return Scope.ofNullable(op())
-    } else {
-      return this;
+    public static ofNullable<U>(value: U | null): Scope<U> {
+        if (value === undefined || value === null) {
+            return new Scope<U>(null)
+        } else {
+            return new Scope<U>(value)
+        }
+    };
+
+    public static suppliedBy<U>(op: () => U): Scope<U> {
+        return Scope.ofNullable(op());
     }
-  };
 
-  ifEmpty = (t: T): Scope<T> => {
-    if (this.empty()) {
-      return Scope.ofNullable(t)
-    } else {
-      return this;
+
+    public static nullableSupplied<U>(op: () => U | null): Scope<U> {
+        return Scope.ofNullable(op());
     }
-  };
 
-  mapTo = <O>(op: (t: T) => (O | null)): Scope<O> => {
-    if (this.nonEmpty()) {
-      return Scope.ofNullable(op(this.value!))
-    } else {
-      return Scope.ofNullable<O>(null)
-    }
-  };
+    get = (): T | null => this.value;
 
-  exists = (): boolean => this.nonEmpty();
-  missing = (): boolean => this.empty();
-  nonEmpty = (): boolean => !this.empty();
-  empty(): boolean {
-    return this.value === null;
-  }
+    getOr = (value: T): T => {
+        if (this.nonEmpty()) {
+            return this.value!
+        } else {
+            return value
+        }
+    };
 
-  static of = <T>(v: T): Scope<T> => Scope.ofNullable(v);
-
-  static ofNullable = <T>(value: T | null): Scope<T> => {
-    if (value === undefined || value === null) {
-      return new Scope<T>(null)
-    } else {
-      return new Scope<T>(value)
-    }
-  };
-
-  static suppliedBy = <T>(op: () => T): Scope<T> => Scope.ofNullable(op());
-
-  static nullableSupplied = <T>(op: () => T | null): Scope<T> => Scope.ofNullable(op());
-
-  get = (): T | null => this.value;
-
-  getOr = (value: T): T => {
-    if (this.nonEmpty()) {
-      return this.value!
-    } else {
-      return value
-    }
-  };
-
-  getOrProvide = (op: () => T): T => {
-    if (this.nonEmpty()) {
-      return this.value!
-    } else {
-      return op()
-    }
-  };
+    getOrProvide = (op: () => T): T => {
+        if (this.nonEmpty()) {
+            return this.value!
+        } else {
+            return op()
+        }
+    };
 }
